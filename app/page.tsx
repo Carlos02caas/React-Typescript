2ca8d505acb73a75e625d2854bf438cb79055421
Carlos Alavez <carlos_caas@hotmail.com}>
2024-01-09 13:39:42 -0600

    Agregando estados y usando componentes con props

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 import Image from 'next/image'
+import { useState } from 'react';
 import { RandomFox } from '../components/RandomFox'
 
+//Funcion genera numero random entre 1 y 123
+const random =() => Math.floor(Math.random()*123)+1;
+
 export default function Home() {
+
+  const [images, setImages] = useState<string[]/*Array<string>*/>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ])
+
   return (
     <main >
       <h1>Hola Platzi</h1>
-      <RandomFox/>
+      {images.map((image,index)=>(
+        <div key={index} className="p-4">
+          <RandomFox image={image}/>
+        </div>
+      ))}
+      
     </main>
   )
 }
--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,7 +1,8 @@
-//Funcion genera numero random entre 1 y 123
-const random =() => Math.floor(Math.random()*123)+1;
+type Props = {
+    image:string
+}
 
-export const RandomFox = ():JSX.Element =>{
-    const image = `https://randomfox.ca/images/${random()}.jpg`
+//export const RandomFox = (props:Props):JSX.Element =>{
+export const RandomFox = ({image}:Props):JSX.Element =>{
     return <img  width={320} height="auto" src={image} className=" rounded"/>
 }
